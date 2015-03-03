e6145cab2340a803ea46202e92386f88ac146d30
Darryl <darrylmurray@me.com>
2015-03-03 02:28:54 -0500

    Heavily refactored and commented javascript.js for cleaner layout, simpler understanding, and broke it up into more understandable pieces.
    Index was updated to keep compatibility with these changes.
    
    jQuery was removed.

--- javascript.js
+++ javascript.js
@@ -1,63 +1,143 @@
-var AccessToken; //contains access token, although isn't really necessary with FB.api
-var members; //contains object holding the members
-var membersList = []; //contains a list only containing the list of users and nothing else
-var adminName = {}; //contains some information about the logged in user.
-var searchRate = 500; //search rate in ms
+//Global variables
+var membersList = {name:[], id:[]}; //contains a list only containing the list of users, ids, and nothing else
+var adminData = {}; //contains some information about the logged in user.
+var searchRate = 500; //search rate in ms per page
+var groupsAllowed = false; //A quick and dirty way of signalling to the entire program whether or not they have the permissions to run
+var fbGroupId = 1554896381413406; //This is the group's ID
+
+//HTML page elements to speed up javascript execution time
+var outputElement;
+var nonExistentElement;
+var errorElement;
+var debugElement;
 
 
-function checkLogin(callback) {
-    FB.getLoginStatus(function(response) {
-        if (response.status === 'connected') {
-            console.log('Logged in.');
-            AccessToken = response.authResponse.accessToken;
-            apiCall("/v2.2/me", function(response) {
-                var objUser = JSON.parse(response);
-                adminName.name = objUser.name;
-                adminName.id = objUser.id;
-                document.getElementById("name").innerHTML= "Welcome, " + adminName.name + "!";
-            });
-            apiCall("/v2.2/me/permissions", function(response) {
-                var groupsAllowed = false;
-                var objPermissions = JSON.parse(response);
-                var stopLoop = false; //to stop the loop prematurely
-                var x = 0;
-                while (!stopLoop || x < objPermissions.data.length) {
-                    if (objPermissions.data[x].permission == "user_groups") {
-                        if (objPermissions.data[x].status == "granted") {
-                            groupsAllowed = true;
-                        }
-                        stopLoop = true;
-                    }
-                    ++x;
+//Global strings
+var reconsiderText = "This app requires permission to view groups in order to administer the ENG Survival group. Please reconsider.<br /><button type=\"button\" onClick=\"reconsider()\">Reconsider</button>";
+var beginCheckTest = "Names loaded. Begin checking? <input type=\"button\" onclick=\"startChecking(0)\" value=\"Yes\">";
+
+
+//Tomiwa Ademidun: 10152756679411139
+
+
+//This function runs once the user has been identified as logged into both Facebook and the app.
+function loggedIn(loginData, callback) {
+
+        //This API call gets the name and needed details of the logged in user, for personalization. Will only run once
+        console.log ("Retrieving logged-in user data...");
+        FB.api("/v2.2/me", function(response) {
+            adminData.name = response.name;
+            adminData.id = response.id;
+            document.getElementById("name").innerHTML= "Welcome, " + adminData.name + "!"; //Write the welcome message to the user
+        });
+
+    checkingGroupPermissions(retrieveNames, askForReconsideration); //Check permissions, act accordingly
+
+    //Starts callback if present
+    if (callback) {
+        callback();
+    }
+}
+
+//This function checks group permissions from the user, and sets the global variable "groupsAllowed" accordingly.
+function checkingGroupPermissions(grantedCallback, deniedCallback) {
+    console.log ("Retrieving permissions...");
+
+    //This API call gets the permissions the user granted the app
+    FB.api("/v2.2/me/permissions", function(objPermissions) {
+
+        //loop through the retrieved permissions data stucture, looking for important bit: groups permissions
+        for (x = 0, stopLoop = false; !stopLoop || x < objPermissions.data.length; x++) {
+            if (objPermissions.data[x].permission === "user_groups") { //if the users_groups section is found...
+                if (objPermissions.data[x].status === "granted") { //... and we're granted permissions to groups...
+                    groupsAllowed = true; //... tell all other functions that we're clear for operation.
+                    console.log ("Groups permissions granted.");
                 }
-                if (groupsAllowed == false) {
-                    document.getElementById("permissions").innerHTML = "This app requires permission to view groups in order to administer the ENG Survival group. Please reconsider.<br /><button type=\"button\" onClick=\"reconsider()\">Reconsider</button>";
+                else { //No groups permissions given
+                    groupsAllowed = false; //unncessary, but good for readability and consistency
+                    console.log ("Groups permissions denied.");
                 }
-                else {
-                    apiCall("/1554896381413406/members?limit=5000&offset=0", function(response){
-                        members = JSON.parse(response); //Note, paging may occur - won't bother controlling for it though
-                        for (x in members.data) {
-                            membersList[x] = members.data[x].name;
-                        }
-                        document.getElementById("members").innerHTML = "Names loaded. Begin checking? \
-<input type=\"button\" onclick=\"startChecking(0)\" value=\"Yes\">";
-                        //adminName.isAdmin = (string === "true");
-                    });
-                }
-                if (callback) {
-                    callback(groupsAllowed);
-                }
-            });
-
-
-
+                stopLoop = true; // stop the loop, we found the permissions for user_groups
+            }
         }
-        else {
-            //leave it to the button to deal with it.
+
+        //Run callback functions if they exist and fit the scenario
+        if (grantedCallback && groupsAllowed === true) {
+            grantedCallback();
+        }
+        else if (deniedCallback && groupsAllowed === false) {
+            deniedCallback();
         }
     });
 }
 
+
+//This function, given appropriate permissions, will start searching the group for member names
+function retrieveNames(limit, offset) {
+
+    //Checking to ensure limits and offsets are valid, chop off decimals
+    if (isNaN(limit) || limit < 1) {
+        limit = 5000;
+    }
+    if (isNaN(offset) || offset < 1) {
+        offset = 0;
+    }
+    limit = Math.floor(limit);
+    offset = Math.floor(offset);
+
+    console.log ("Limit: " + limit + " Offset: " + offset); //output settings of run
+
+    if (groupsAllowed === true) {
+        console.log ("Retrieving member names...");
+        
+        //This function retrieves the member list page
+        FB.api("/" + fbGroupId + "/members?limit=" + limit + "&offset=" + offset, function(response){
+            //Iterates through member list, recording names
+            for (x in response.data) {
+                membersList.name.push(response.data[x].name);
+                membersList.id.push (response.data[x].id);
+            }
+            //If there are more pages to go, retrieve them. Else, give the user heads up that we're ready to start.
+            if (response.paging.next != null) {
+                console.log("Next page...");
+                retrieveNames(limit, membersList.name.length);
+            }
+            else {
+                console.log ("Done retrieving names.");
+                document.getElementById("members").innerHTML = beginCheckTest;
+            }
+        });
+    }
+}
+
+//Given insufficient permissions, this function will ask for them again.
+function askForReconsideration() {
+    if (groupsAllowed === false) {
+        console.log("Asking for reconsideration...");
+        document.getElementById("permissions").innerHTML = reconsiderText;
+    }
+}
+
+//This function coordinates re-asking the user for group permissions.
+function reconsider() {
+    console.log ("Asking for permissions again...");
+    FB.login(
+        function(response) {
+            console.log("Checking if reconsideration was successful... wipe reconsider message, will reappear if needed.");
+            document.getElementById("permissions").innerHTML = "";
+            checkLogin(loggedIn);
+
+        },
+        {
+            scope: 'user_groups',
+            auth_type: 'rerequest'
+        }
+    );
+}
+
+
+
+//This is the checking function.
 function startChecking(entry) {
     console.log("StartChecking run " + entry + " of " + membersList.length);
     if (entry < membersList.length) {
@@ -102,60 +182,48 @@ function startChecking(entry) {
 }
 
 
-function reconsider() {
-    FB.login(
-        function(response) {
-            console.log("Checking...");
-            checkLogin(function (groupsAllowed) {
-                if (groupsAllowed == true) {
-                    console.log("Check done");
-                    document.getElementById("permissions").innerHTML = ""; //members variable is now stocked with real users, wipe permissions warning.
-                }
-            });
 
-        },
-        {
-            scope: 'user_groups',
-            auth_type: 'rerequest'
-        }
-    );
-}
 
-//for all FB API calls
-function apiCall(url, callback) {
-    FB.api(url, function(response) {
-        //console.log(JSON.stringify(response));
-        if (callback) {
-            callback(JSON.stringify(response));
-        }
-    });
-}
 
-function httpGet(theUrl) {
+//Depreciated. Keep it though?
+/*function httpGet(theUrl) {
     var xmlHttp = null;
 
     xmlHttp = new XMLHttpRequest();
     xmlHttp.open( "GET", theUrl, false );
     xmlHttp.send( null );
     return xmlHttp.responseText;
-}
+}*/
 
-//FB.Init is up verification code. The rest of the FB functions will not run until fbApiInit = true;
-var fbApiInit = false;
-function checkAPIComplete(globalControlVariable, callback) {
+//No other javascript will run until we know the FB login status of the user.
+//This function also verifies if a login was made successfully by the facebook login button
+function checkLogin(successCallback, unauthorizedCallback, unknownCallback) {
+
+    //This block will make sure that there are no empty callback functions
+    successCallback = successCallback || function() {console.log("Empty sucessCallback, err. 0001.");};
+    unauthorizedCallback = unauthorizedCallback || function() {console.log("Empty unauthorizedCallback, err. 0002.");};
+    unknownCallback = unknownCallback || function() {console.log("Empty unknownCallback, err. 0003.");};
+
     FB.getLoginStatus(function(response){
-        globalControlVariable = true;
-    });
-    if (!globalControlVariable) {
-        setTimeout(function() {checkAPIComplete(globalControlVariable,callback);}, 50);
-    }
-    else {
-        if(callback) {
-            callback();
+        console.log ("FB API initialization complete.");
+
+        if (response.status === "connected") {
+            console.log ("Connected.");
+            successCallback(response);
         }
-    }
+        else if (response.status === "not_authorized") {
+            console.log ("Not authorized.");
+            unauthorizedCallback(response);
+        }
+        else {
+            console.log ("Not logged into Facebok.");
+            unknownCallback(response);
+        }
+    });
 }
 
+
+//Don't touch anything below this (exception: jQuery), it's secret Facebook stuff...
 window.fbAsyncInit = function() {
     FB.init({
         appId      : '1461836634038781',
@@ -165,9 +233,16 @@ window.fbAsyncInit = function() {
         version    : 'v2.1'
     });
 
-    $(document).ready(function() {
-        checkAPIComplete(fbApiInit, checkLogin);
-    }); //This blocks the initial run of the javascript until jQuery is ready.
+    //HTML page elements to speed up javascript execution time
+    outputElement = document.getElementById("output");
+    nonExistentElement = document.getElementById("nonExistent");
+    errorElement = document.getElementById("error");
+    debugElement = document.getElementById("debug");
+
+    
+    //If jQuery was to be implemented, it would be placed here to block initial javascript execution until jQuery is ready.
+    checkLogin(loggedIn);
+
 };
 
 (function(d, s, id){
